b2e87cec939b972b1ad6bacadbf1905263764765
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-14 16:29:38 -0600

    added fetch to spotify api

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,32 @@ const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
+  // initialize search input from POST request
   const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  // fetch access token
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+      "grant_type=client_credentials&client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret,
+    };
+  
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+      // [] makes sure it runs only once
+  }, []);
+
   return (
    <>
       <Container>
