4441bfbbd55d1b38f4c7e8cddd5dce2da866a8da
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-14 15:53:09 -0600

    added clientid and clientsecret for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
 import './App.css'
 
+// declare keys from .env file
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+console.log(clientId, clientSecret);
+
 function App() {
   
 
